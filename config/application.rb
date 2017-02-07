282dc9e1bfac88e5e421762b2a0e79b2c694b47e
Marc-André Giroux <mgiroux0@gmail.com>
2017-02-07 18:36:52 -0500

    rm mutation autoload path

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module Swapi
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths << Rails.root.join('app', 'graph')
-    config.autoload_paths << Rails.root.join('app', 'graph', 'mutations')
   end
 end
