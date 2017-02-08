b6ebf017d8046ee6dd362f0595c19382f7797164
Christian Joudrey <cmallette@gmail.com>
2017-02-08 00:04:48 -0500

    Add Person model and fixtures

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,3 @@
 class Planet < ApplicationRecord
+  has_many :residents, class_name: 'Person', foreign_key: 'homeworld_id'
 end
---
+++ db/migrate/20170208051339_create_people.rb
@@ -0,0 +1,17 @@
+class CreatePeople < ActiveRecord::Migration[5.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :birth_year
+      t.string :eye_color
+      t.string :gender
+      t.string :hair_color
+      t.integer :height
+      t.integer :mass
+      t.string :skin_color
+      t.references :homeworld, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
