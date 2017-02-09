25698aef6a32d1db90a64ae6a63cdbafdd21ffb8
Christian Joudrey <cmallette@gmail.com>
2017-02-08 19:38:29 -0500

    Add starship pilots

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,5 @@
 class Person < ApplicationRecord
   belongs_to :homeworld, class_name: 'Planet'
+
+  has_and_belongs_to_many :starships
 end
--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,2 +1,7 @@
 class Starship < ApplicationRecord
+  has_and_belongs_to_many :pilots,
+    class_name: 'Person',
+    join_table: 'people_starships',
+    foreign_key: 'starship_id',
+    association_foreign_key: 'person_id'
 end
