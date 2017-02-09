9ecda7eda1147cbdcd592fcc022804e50be439fd
Christian Joudrey <cmallette@gmail.com>
2017-02-09 12:51:50 -0500

    Add Vehicle#pilots, Person#vehicles associations and fixtures

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,7 @@
 class Vehicle < ApplicationRecord
+  has_and_belongs_to_many :pilots,
+    class_name: 'Person',
+    join_table: 'people_vehicles',
+    foreign_key: 'vehicle_id',
+    association_foreign_key: 'person_id'
 end
