a195236eb4a38896ca9ea65c5f62d4954e003c2b
Marc-André Giroux <mgiroux0@gmail.com>
2017-02-11 14:25:29 -0500

    policial :troll:

--- app/models/graph/schema.rb
+++ app/models/graph/schema.rb
@@ -6,13 +6,13 @@ module Graph
       Graph::Schema.types.values.find { |type| type.name == obj.class.name }
     end
 
-    id_from_object ->(object, type_definition, query_ctx) {
+    id_from_object ->(object, type_definition, query_ctx) do
       URI::GID.build(app: GlobalID.app, model_name: type_definition.name, model_id: object.id)
-    }
+    end
 
-    object_from_id ->(id, query_ctx) {
+    object_from_id ->(id, query_ctx) do
       gid = GlobalID.parse(id)
       Object.const_get(gid.model_name).find(gid.model_id)
-    }
+    end
   end
 end
