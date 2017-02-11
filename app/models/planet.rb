ae8042661fbab95dba525dcc2774c8debdca0d58
Christian Joudrey <cmallette@gmail.com>
2017-02-11 14:09:08 -0500

    Add Film associations to models

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,3 +1,4 @@
 class Planet < ApplicationRecord
   has_many :residents, class_name: 'Person', foreign_key: 'homeworld_id'
+  has_and_belongs_to_many :films
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -4,4 +4,5 @@ class Vehicle < ApplicationRecord
     join_table: 'people_vehicles',
     foreign_key: 'vehicle_id',
     association_foreign_key: 'person_id'
+  has_and_belongs_to_many :films
 end
