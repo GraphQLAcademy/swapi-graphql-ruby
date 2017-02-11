8838a13651894748ad08f28f0e08b9a908bfd622
Marc-André Giroux <mgiroux0@gmail.com>
2017-02-11 16:42:42 -0500

    GraphiQL in production too

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql"
-  end
-
+  mount GraphiQL::Rails::Engine, at: "/", graphql_path: "/graphql"
   post '/graphql', to: 'graphql#execute'
 end
