dab0516f1294e5d0ded92b15f7c9b590c7323c06
Marc-André Giroux <mgiroux0@gmail.com>
2017-02-12 15:08:33 -0500

    add email to user

--- db/migrate/20170212195638_create_ratings.rb
+++ db/migrate/20170212195638_create_ratings.rb
@@ -2,6 +2,7 @@ class CreateRatings < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :email
       t.timestamps
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -149,6 +149,7 @@ ActiveRecord::Schema.define(version: 20170212195638) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
