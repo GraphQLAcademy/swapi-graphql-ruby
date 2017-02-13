27075ca73fd34f4485f7e3e77bcf16a2b9a4dc71
Marc-André Giroux <mgiroux0@gmail.com>
2017-02-12 23:26:56 -0500

    GraphiQL settings

--- config/initializers/graphiql.rb
+++ config/initializers/graphiql.rb
@@ -1,6 +1,10 @@
 require "base64"
 
+GraphiQL::Rails.config.initial_query = "query { allFilms { edges { node { title } } } }"
+
 GraphiQL::Rails.config.headers['Authorization'] = -> (context) do
   credentials = Base64.strict_encode64("#{context.params['username']}:#{context.params['password']}")
   "basic #{credentials}"
 end
+
+GraphiQL::Rails.config.query_params = true
