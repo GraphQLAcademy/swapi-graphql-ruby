75a97043e13790988b7e7a59617f33f676b1d92e
Christian Joudrey <cmallette@gmail.com>
2017-02-12 23:53:14 -0500

    Fix merge conflict

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -2,9 +2,6 @@ class GraphqlController < ApplicationController
   before_action :authenticate
 
   def execute
-    context = {
-      user: @user,
-    }
     query_string = params[:query].to_s
     variables = ensure_hash(params[:variables])
     context = {
